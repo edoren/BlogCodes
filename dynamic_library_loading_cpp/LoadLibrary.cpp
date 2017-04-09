6d20ce3b3f919e0931aeafe020a87c06ce9c79e9
Manuel Sabogal <mfer32@gmail.com>
2017-04-09 00:55:43 -0500

    Fixed print message in LoadLibrary.cpp

--- dynamic_library_loading_cpp/LoadLibrary.cpp
+++ dynamic_library_loading_cpp/LoadLibrary.cpp
@@ -6,20 +6,19 @@ typedef void (*PFN_SAY_NAME)(const char*);
 
 int main(void) {
     void* handle = dlopen("libHelloLibrary.so", RTLD_LAZY);
-
-    if (!handle) {    
-        std::cout << "Could not open the math library" << std::endl;
+    if (!handle) {
+        std::cout << "Could not open the library" << std::endl;
         return 1;
     }
 
-    PFN_SAY_NAME hello = (PFN_SAY_NAME)dlsym(handle, "SayHello");
+    PFN_SAY_NAME hello = reinterpret_cast<PFN_SAY_NAME>(dlsym(handle, "SayHello"));
     if (!hello) {
         std::cout << "Could not find symbol SayHello" << std::endl;
         dlclose(handle);
         return 1;
     }
 
-    hello("JointDeveloper");
+    hello("Edoren");
     dlclose(handle);
 
     return 0;
